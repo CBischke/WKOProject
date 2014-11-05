e818cac3ea38ad2645db241eaac0f1201418733f
CBischke <CBischke10@winona.edu>
2014-11-05 16:33:13 -0600

    Changing the color
    Changed a lot of shit

--- footer.php
+++ footer.php
@@ -1,15 +1,4 @@
 <div id="footer">
 	<p>We are not a real company. I don't even get paid. I am a webdeveloper that was forced against my will to develop this website. I am currently in the basement of the CEOs mother's house. Please help. It smells like burnt tomato soup. If you are still reading this, I commend you. No, I applaud  you. Why are you still reading this? Obviously I am not going anywhere with this though. Placing a random &copy; sign makes this footer look official. Yeah. That looks good. &copy;
 	<span id="secret">Oh good. You found this line. Please stop reading the footer. I am running out of things to talk about.</span>
-	</p>
-	<p>
-	    <a href="http://jigsaw.w3.org/css-validator/check/referer">
-	        <img style="border:0;width:88px;height:31px"
-	            src="http://jigsaw.w3.org/css-validator/images/vcss"
-	            alt="Valid CSS!" />
-	    </a>
-	</p>
-	<a href="http://www.w3.org/html/logo/">
-		<img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-semantics.png" width="165" height="64" alt="HTML5 Powered with CSS3 / Styling, and Semantics" title="HTML5 Powered with CSS3 / Styling, and Semantics">
-	</a>
 </div>
