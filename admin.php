2ad1294e4cfe2e4c6db10222e71162efe9fe756c
CBischke <CBischke10@winona.edu>
2014-11-10 12:39:22 -0600

    adding admin page
    Added the admin page to do admin things

---
+++ admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="util.js"></script>
+</head>
+<body>
+	<div id="bodyContainer">
+
+		<?php include 'menu.php';?>
+		
+		<div class="clear"></div>
+		<div id="mainBody">
+			<div class="mainCenter">
+				<!--PUT TEXT AREA HERE-->
+			</div>
+		</div>
+		<div class="clear"></div>
+		<?php include 'footer.php';?>
+	</div>
+</body>
+</html>
